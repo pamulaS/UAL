872fdb372097777b69e82dd236a450ee72ed9d70
pamulaS <39582950+pamulaS@users.noreply.github.com>
2019-06-10 19:18:54 +0530

    Update Tealium_script.js

--- Tealium_script.js
+++ Tealium_script.js
@@ -1,13 +1,15 @@
-var Utag_Data = (function(){
-var scriptUDOTag = document.createElement('script');
-var scriptUDOData = document.createTextNode('var utag_data = {}');
-scriptUDOTag.appendChild(scriptUDOData);
-if(document.getElementsByTagName('body')){
-document.body.insertBefore(scriptUDOTag, document.body.firstChild)
-
+window.onload = function(){
+    if(document.getElementsByTagName('body')){  
+        var UDO_Obj = document.createElement('script');
+        var UDO_Obj_Data = document.createTextNode('var utag_data = {}');
+        UDO_Obj.appendChild(UDO_Obj_Data);
+        var tealium_lib= document.createElement('script');
+        var tealium_lib_Data = document.createTextNode("(function(a,b,c,d){a='//tags.tiqcdn.com/utag/unitedairlines/sandbox/prod/utag.js';b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);})()");
+        tealium_lib.appendChild(tealium_lib_Data);
+        document.body.insertBefore(tealium_lib, document.body.firstChild)
+        document.body.insertBefore(UDO_Obj, document.body.firstChild)
+        }
 }
-}());
-export default Utag_Data;
 
 
 
